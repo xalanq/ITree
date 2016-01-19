b6ac36edff328c0f7a008424320389ed2ee02162
iwtwiioi <iwtwiioi@gmail.com>
2016-01-20 03:18:15 +0800

    Alpha 1.3 修复markdown的公式问题.

--- imarkdown.py
+++ imarkdown.py
@@ -2,12 +2,79 @@
 
 # markdown stuff
 
-import mistune
+import mistune, re
+
+# Thanks https://github.com/jupyter/nbconvert/blob/master/nbconvert/filters/markdown_mistune.py
+
+class MathBlockGrammar(mistune.BlockGrammar):
+	block_math = re.compile(r"^\$\$(.*?)\$\$", re.DOTALL)
+	latex_environment = re.compile(r"^\\begin\{([a-z]*\*?)\}(.*?)\\end\{\1\}",
+												re.DOTALL)
+
+
+class MathBlockLexer(mistune.BlockLexer):
+	default_rules = ['block_math', 'latex_environment'] + mistune.BlockLexer.default_rules
+
+	def __init__(self, rules=None, **kwargs):
+		if rules is None:
+			rules = MathBlockGrammar()
+		super(MathBlockLexer, self).__init__(rules, **kwargs)
+
+	def parse_block_math(self, m):
+		"""Parse a $$math$$ block"""
+		self.tokens.append({
+			'type': 'block_math',
+			'text': m.group(1)
+		})
+
+	def parse_latex_environment(self, m):
+		self.tokens.append({
+			'type': 'latex_environment',
+			'name': m.group(1),
+			'text': m.group(2)
+		})
+
+
+class MathInlineGrammar(mistune.InlineGrammar):
+	math = re.compile(r"^\$(.+?)\$", re.DOTALL)
+	block_math = re.compile(r"^\$\$(.+?)\$\$", re.DOTALL)
+	text = re.compile(r'^[\s\S]+?(?=[\\<!\[_*`~$]|https?://| {2,}\n|$)')
+
+
+class MathInlineLexer(mistune.InlineLexer):
+	default_rules = ['block_math', 'math'] + mistune.InlineLexer.default_rules
+
+	def __init__(self, renderer, rules=None, **kwargs):
+		if rules is None:
+			rules = MathInlineGrammar()
+		super(MathInlineLexer, self).__init__(renderer, rules, **kwargs)
+
+	def output_math(self, m):
+		return self.renderer.inline_math(m.group(1))
+
+	def output_block_math(self, m):
+		return self.renderer.block_math(m.group(1))
+
+
+class MarkdownWithMath(mistune.Markdown):
+	def __init__(self, renderer, **kwargs):
+		if 'inline' not in kwargs:
+			kwargs['inline'] = MathInlineLexer
+		if 'block' not in kwargs:
+			kwargs['block'] = MathBlockLexer
+		super(MarkdownWithMath, self).__init__(renderer, **kwargs)
+
+	def output_block_math(self):
+		return self.renderer.block_math(self.token['text'])
+
+	def output_latex_environment(self):
+		return self.renderer.latex_environment(self.token['name'], self.token['text'])
+
 
 class IMarkdownRenderer(mistune.Renderer):
 	""" renderer of markdown """
-	def __init__(self):
-		super().__init__()
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 
 	def link(self, link, title, text):
 		"""
@@ -76,19 +143,31 @@ class IMarkdownRenderer(mistune.Renderer):
 		cmd += ' width="{}"'.format(width) if width else ''
 		cmd += ' height="{}"'.format(height) if height else ''
 		# print(cmd)
-		return '<img {} onerror="imageRenderer.processing(this); imageRenderer.picture.assignTo(this);">'.format(cmd)
 		# return '<img {} onerror="alert(imageRenderer.processing(this));">'.format(cmd)
+		return '<img {} onerror="imageRenderer.processing(this); imageRenderer.picture.assignTo(this);">'.format(cmd)
+
+	# Pass math through unaltered - mathjax does the rendering in the browser
+	def block_math(self, text):
+		return '$$%s$$' % text
+
+	def latex_environment(self, name, text):
+		return r'\begin{%s}%s\end{%s}' % (name, text, name)
+
+	def inline_math(self, text):
+		return '$%s$' % text
 
 
 class IMarkdown(mistune.Markdown):
 	""" ************************************************* markdown ************************************************* """
 	def __init__(self):
 		self.renderer = IMarkdownRenderer()
-		super().__init__(renderer=self.renderer, hard_wrap=True)
+		super().__init__(renderer=self.renderer, inline=MathInlineLexer(self.renderer), block=MathBlockLexer(), hard_wrap=True)
 
 
 def main():
-	pass
+	m = IMarkdown()
+	print(m('abc $ a * b * c $ efg'))
+
 
 if __name__ == '__main__':
 	main()
